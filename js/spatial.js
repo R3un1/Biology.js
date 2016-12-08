81f2be527b13d8b5670f033af03bd69759c3a63c
James Bohacek <jamesbohacek@outlook.com>
2016-12-08 23:17:58 +0100

    minor changes

--- js/spatial.js
+++ js/spatial.js
@@ -121,6 +121,6 @@ function writeF() {
 onmessage = function(c) {
     gridLenght = c.data[0];
     maxMatingDistance = c.data[1];
-    console.log('message received')
     initGrid();
+    postMessage(grid);
 };
